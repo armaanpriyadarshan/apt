b1e6424848687cc1a75633b848e6357f2090517a
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2026-09-06 16:39:41 -0700

    rmsnorm

--- apt/nn.py
+++ apt/nn.py
@@ -21,3 +21,20 @@ class Embedding(nn.Module):
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.weight[token_ids]
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float=1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.weight = torch.nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+
+        rms = torch.sqrt(1 / self.d_model * torch.sum(x ** 2, dim=-1, keepdim=True) + self.eps)
+        result = x / rms * self.weight
+
+        return result.to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -11,7 +11,7 @@ from torch import Tensor
 
 from apt.bpe import train_bpe
 from apt.tokenizer import Tokenizer
-from apt.nn import Linear, Embedding
+from apt.nn import Linear, Embedding, RMSNorm
 
 
 def run_linear(
@@ -386,7 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model=d_model, eps=eps)
+    rmsnorm.load_state_dict({"weights": weights})
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
